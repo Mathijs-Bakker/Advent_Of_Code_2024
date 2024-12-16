17fc731c9934e13c829946a1c5ee90af3cd813e6
mathijs-bakker <mathijs_bakker@me.com>
2024-12-16 20:55:35 +0100

    Solution Part One and Two

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,5 +1,12 @@
-fn main() {
-    let data = include_str!("../data/example_input.txt");
+mod part_one;
+mod part_two;
 
-    println!("{data}");
+fn main() {
+    let data = include_str!("../data/input.txt");
+
+    let result = part_one::calc_sum_of_multiplications(data);
+    println!("Part one: {:?}", result);
+
+    let result = part_two::Data::new(data).next().unwrap();
+    println!("Part two: {:?}", result);
 }
